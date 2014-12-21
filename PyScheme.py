609bc38c36fd5808120846abb8aafee8647f20d1
Aashish Satyajith <ankarathaashish@gmail.com>
2014-12-22 01:48:50 +0530

    Added tokenize function

--- PyScheme.py
+++ PyScheme.py
@@ -9,3 +9,19 @@ Description: In this project I attempt to write an implementation of a subset
 of the Scheme Programming Language in Python.
 """
 
+# we'll need functions that perform basic housekeeping
+# (removing all the parens for processing etc.)
+
+def tokenize(string):
+    
+    """
+    Converts a string into a list of tokens.
+    """
+    
+    # add a space to parens so that they can be split easily
+    string = string.replace('(', ' ( ')
+    string = string.replace(')', ' ) ')
+    
+    return string.split()
+    
+    
