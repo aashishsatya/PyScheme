8da2f25a39070f996cc16a2f288ef303eb3832fd
Aashish Satyajith <ankarathaashish@gmail.com>
2014-12-22 01:09:20 +0530

    First commit

---
+++ PyScheme.py
@@ -0,0 +1,11 @@
+"""
+Created on Sun Dec 21 23:53:09 2014
+
+@author: aashishsatya
+
+Project Name: PyScheme
+
+Description: In this project I attempt to write an implementation of a subset
+of the Scheme Programming Language in Python.
+"""
+
