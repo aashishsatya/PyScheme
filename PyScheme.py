29e03a91289bec1f97f8c227ddeeaa424d99b30b
Aashish Satyajith <ankarathaashish@gmail.com>
2014-12-22 02:03:44 +0530

    Adding environments

--- PyScheme.py
+++ PyScheme.py
@@ -9,6 +9,9 @@ Description: In this project I attempt to write an implementation of a subset
 of the Scheme Programming Language in Python.
 """
 
+# initially we will ignore strings and comments and will get just the core
+# functionality working
+
 # we'll need functions that perform basic housekeeping
 # (removing all the parens for processing etc.)
 
@@ -23,5 +26,20 @@ def tokenize(string):
     string = string.replace(')', ' ) ')
     
     return string.split()
+
+# to implement the environment model of evaluation in Scheme, 
+# we need environments. 
+class Env(dict):
+    """
+    An environment is simply a dict of variable-value pairs, and some
+    predefined mappings.
+    """
+       
+def eval(exp, env):
+    
+    """
+    Evaluates the expression exp in the given environment env
+    and return the result
+    """
     
     
