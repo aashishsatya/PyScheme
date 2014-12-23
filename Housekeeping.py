118f3b692db30f4fdd24410b9664a7f0201aa287
Aashish Satyajith <ankarathaashish@gmail.com>
2014-12-23 18:19:48 +0530

    Added parse function

--- Housekeeping.py
+++ Housekeeping.py
@@ -19,8 +19,10 @@ Modeled after Peter Norvig's implementation of the same.
 
 def tokenize(string):
     
+    # a token is the smallest individual unit of a program
+    
     """
-    Converts a string into a list of tokens.
+    Converts the input string into a list of tokens.
     """
     
     # add a space to parens so that they can be split easily
@@ -28,4 +30,56 @@ def tokenize(string):
     string = string.replace(')', ' ) ')
     
     return string.split()
+    
+# we will model things exactly as with the scheme implementation
+# of the metacircular evaluator
+# (define a 10) becomes ['define', 'a', '10']
+# To make the concept more clear, think of a list in Scheme as a 
+# list in Python!!
+# this lets the classifier do its work (see Classifier.py)
+
+# Thanks Peter Norvig for making this part easy
+
+def parse(program):
+    
+    """
+    Calls tokenize and readFromTokens, cleans up the program for processing 
+    as mentioned above.
+    """
+    
+    # assumes the programmer to be correct
+    
+    return readFromTokens(tokenize(program))
+    
+def readFromTokens(tokenList):    
+    
+    """
+    Identifies individual expressions from a list of tokens and packages
+    them as a list
+    """
+    
+    # this is implemented as a separate function for the recursion to work
+    # properly
+    
+    firstToken = tokenList.pop(0)
+    
+    if firstToken == '(':
+        # new expression in place
+        # so initialize new list to package it
+        newExpression = []
+        while tokenList[0] != ')':
+            # keep appending values to the new expression list
+            newExpression.append(readFromTokens(tokenList))
+        # like Mr. Norvig said, remove  the ')'
+        tokenList.pop(0)
+        return newExpression
+    else:
+        # token is not the start of a new expression
+        # i.e. already in its smallest form
+        # so simply return
+        return firstToken
+    
+    
+    
+
 
